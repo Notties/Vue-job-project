bfaab807461051ba4e5fb1a157677b53bf29db42
Knot <akthakorn@gmail.com>
2024-11-11 16:01:29 +0700

    fix: assets links

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vue-job-project/',
   publicPath: '/Vue-job-project/',
   plugins: [vue()],
   server: {
